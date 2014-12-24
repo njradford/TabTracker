6cc7cee6c288a2e1e3c4942fb00e6272f5b38b69
Nick <nicktheradford@gmail.com>
2014-12-24 14:06:31 -0800

    Removed responsive stylings

--- Options_Page/test.js
+++ Options_Page/test.js
@@ -1,3 +1,45 @@
 /**
  * Created by Nick on 12/23/2014.
  */
+$(function() {
+
+    console.log("LOADED");
+
+    $(".graph").click(function(){
+
+        $(this).parent().parent().find(".graphPane").slideToggle(800);
+
+
+        var data = {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    label: "My First dataset",
+                    fillColor: "rgba(220,220,220,0.2)",
+                    strokeColor: "rgba(220,220,220,1)",
+                    pointColor: "rgba(220,220,220,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(220,220,220,1)",
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                },
+                {
+                    label: "My Second dataset",
+                    fillColor: "rgba(151,187,205,0.2)",
+                    strokeColor: "rgba(151,187,205,1)",
+                    pointColor: "rgba(151,187,205,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(151,187,205,1)",
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
+        };
+        Chart.defaults.global.responsive = true;
+        var ctx = document.getElementById("chart").getContext("2d");
+        var myLineChart = new Chart(ctx).Line(data);
+
+
+
+    })
+});
